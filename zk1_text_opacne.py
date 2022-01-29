04d6968579bff316d7293ad5dbbd5ec7365df662
Pepa Zátka <zatkaj@natur.cuni.cz>
2022-01-29 22:54:11 +0100

    rozpracovana_velka_pismena

--- zk1_text_opacne.py
+++ zk1_text_opacne.py
@@ -1,17 +1,33 @@
+from pickle import TRUE
+
+
 def otoc_seznam(seznam):
     novy_seznam = seznam[::-1]
     return novy_seznam
 
-def konec_vety(li):
+def konec_vety_nebo_carka(li):
     novy_li = []
     for a in li:
         i = list(a)
-        if i[-1] == "." or i[-1] == "!" or i[-1] == "?":
+        if i[-1] == "." or i[-1] == "!" or i[-1] == "?" or i[-1] == ",":
             i.insert(0, i[-1])
             del i[-1]
             a = ''.join(i)
         novy_li.append(a)
     return(novy_li)
+
+def velke_pismeno(li):
+    novy_li = []
+    for a in li:
+        i = list(a)
+        if i[0].isupper() == True:
+            
+            a = ''.join(i)
+        novy_li.append(a)
+    return(novy_li)
+
+
+
 
 text = input("Vložte text:")
 print(text)
@@ -19,5 +35,5 @@ text_list = text.split()
 print(text_list)
 text_list_opacne = otoc_seznam(text_list)
 print(text_list_opacne)
-text_list_opacne_konce = konec_vety(text_list_opacne)
-print(text_list_opacne_konce)
+text_list_opacne_uprava = konec_vety_nebo_carka(text_list_opacne)
+print(text_list_opacne_uprava)
