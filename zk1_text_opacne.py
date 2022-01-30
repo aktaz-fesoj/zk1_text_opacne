5991471663812af7ad045b75ff92ca51c86e97a3
Pepa Zátka <zatkaj@natur.cuni.cz>
2022-01-30 18:22:29 +0100

    zapis_vystupniho_textu

--- zk1_text_opacne.py
+++ zk1_text_opacne.py
@@ -29,17 +29,25 @@ def nacti_data(adresa):
         text_read = vstupni_text.read()
     return(text_read)
 
+def zapis_text(adresa, text_k_zapsani):
+    with open(adresa, "w", encoding = "utf-8") as vystupni_text:
+        vystupni_text.write(text_k_zapsani)
+    return(f"Úspěšně uložen soubor {adresa} s textem s obráceným pořadím slov.")
+
 
 try:
     text = nacti_data("text.txt")
 except:
-    text = input("Vložte text:")
+    text = input("Soubor text.txt nebyl nalezen ve stejné složce, ze které spouštíte tento program. Můžete vložit text ručně:")
 print(text)
-vp = input("Přejete si upravit velikost písmen pro čtení zprava doleva? Zadejte 1 pro volbu ANO.")
+vel_pismena_ano = input("Přejete si upravit velikost písmen pro čtení zprava doleva? Zadejte 1 pro volbu ANO.")
 text_list = text.split()
 text_f = otoc_seznam(text_list)
 text_f = konec_vety_nebo_carka(text_f)
-if vp == "1":
+if vel_pismena_ano == "1":
     text_f = velke_pismeno(text_f)
 text_f_spojeny = ' '.join(text_f)
 print(text_f_spojeny)
+zapis_ano = input("Přejete si zapsat upravený výsledný text do nového textového souboru? Zadejte 1 pro volbu ANO.")
+if zapis_ano == "1":
+    zapis_text("obraceno.txt", text_f_spojeny)
