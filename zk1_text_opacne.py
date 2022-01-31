c1a9bbbe42c54baa4e20325dfa3b22ce2a664046
Pepa Zátka <zatkaj@natur.cuni.cz>
2022-01-31 19:35:17 +0100

    Dokumentace, drobnosti

--- zk1_text_opacne.py
+++ zk1_text_opacne.py
@@ -1,19 +1,42 @@
 def otoc_seznam(seznam):
-    novy_seznam = seznam[::-1]
+    """Funkce obrátí pořádí prvků seznamu.
+
+        Parameters:
+                    seznam(list): Vstupní seznam, který bude otočen
+                    novy_seznam(list): Výstupní seznam s opačným pořadím prvků oproti seznamu vstupnímu
+    """
+    novy_seznam = seznam[::-1]      # Step parameter -1 docílí "braní" prvků v opačném pořadí
     return novy_seznam
 
 def konec_vety_nebo_carka(li):
+    """Funkce upraví položky v seznamu. Pokud se na konci řetězce nachází alespoň jeden ze znaků .,;?! bude tento znak(y) převeden na druhou stranu textového řetězce (pokud se zde nachází znaků více, budou převedeny v opačném pořadí).
+
+        Parameters:
+                    li(list): Vstupní seznam, který bude upraven
+                    novy_li(list): Výstupní upravený seznam
+    """
     novy_li = []
     for a in li:
-        i = list(a)
-        if i[-1] == "." or i[-1] == "!" or i[-1] == "?" or i[-1] == ",":
-            i.insert(0, i[-1])
-            del i[-1]
-            a = ''.join(i)
+        i = list(a)                 #Rozdělení slova na znaky
+        znovu = True
+        while znovu == True:        #Následující část se opakuje dokud na konci slova není žádný "nevhodný" znak
+            if i[-1] == "." or i[-1] == "!" or i[-1] == "?" or i[-1] == "," or i[-1] == ";":
+                i.insert(0, i[-1])  #Vložení daného znaku na první pozici řetězce (=před řetězec)
+                del i[-1]           #Pdstranění znaku z konce řetězce
+                a = ''.join(i)      #Opětovné spojení znaků do slova
+            else:
+                znovu = False
+
         novy_li.append(a)
-    return(novy_li)
+    return novy_li
 
 def velke_pismeno(li):
+    """Funkce upraví položky v seznamu. Začíná-li textový řetězec velkým písmenem, bude toto písmeno zmenšeno a naopak zvětšen bude poslední znak řetězce.
+
+        Parameters:
+                    li(list): Vstupní seznam, ve kterém budou upravena počáteční velká písmena
+                    novy_li(list): Výstupní upravený seznam
+    """
     novy_li = []
     for a in li:
         i = list(a)
@@ -22,14 +45,26 @@ def velke_pismeno(li):
             i[-1] = i[-1].upper()
             a = ''.join(i)
         novy_li.append(a)
-    return(novy_li)
+    return novy_li
 
 def nacti_data(adresa):
+    """Funkce načte textový řetězec ze souboru a uloží ho do proměnné.
+
+        Parameters:
+                    adresa(str): Absolutní či relativní cesta k zdrojovému textovému souboru
+                    text_read(str): Textový řetězec obsahující text ze vstupního textového souboru
+    """
     with open(adresa, encoding = "utf-8") as vstupni_text:
         text_read = vstupni_text.read()
     return(text_read)
 
 def zapis_text(adresa, text_k_zapsani):
+    """Funkce uloží text do textového souboru.
+
+        Parameters:
+                    adresa(str): Absolutní či relativní cesta k souboru, do kterého má být text zapsán
+                    text_k_zapsani(str): Text, který bude do výstupního souboru zapsán
+    """
     with open(adresa, "w", encoding = "utf-8") as vystupni_text:
         vystupni_text.write(text_k_zapsani)
     return(f"Úspěšně uložen soubor {adresa} s textem s obráceným pořadím slov.")
@@ -38,7 +73,7 @@ def zapis_text(adresa, text_k_zapsani):
 try:
     text = nacti_data("text.txt")
 except:
-    text = input("Soubor text.txt nebyl nalezen ve stejné složce, ze které spouštíte tento program. Můžete vložit text ručně:")
+    text = input("Soubor text.txt nebyl nalezen ve stejné složce, ze které spouštíte tento program, nebo se ho nepodařilo úspěšně spustit. Můžete vložit text ručně:")
 print(text)
 vel_pismena_ano = input("Přejete si upravit velikost písmen pro čtení zprava doleva? Zadejte 1 pro volbu ANO.")
 text_list = text.split()
