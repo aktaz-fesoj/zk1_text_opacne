fc7c0a127d706fab4b21974510cfa4e943f11ede
Pepa Zátka <zatkaj@natur.cuni.cz>
2022-02-10 10:24:55 +0100

    delete reverse copying + while simplification

--- zk1_text_opacne.py
+++ zk1_text_opacne.py
@@ -4,11 +4,11 @@ def list_reverse(list1):
         Parameters:
                     list1(list1): The input list that has to be reversed
         Returns:
-                    new_list(list): Output list with elements in reversed order in comparison with the input list.
+                    new_list(list1): Output list with elements in reversed order in comparison with the input list.
     """
     # Elements are going to be "taken" in reverse order due to step parameter -1
-    new_list = list1[::-1]
-    return new_list
+    list1 = list1[::-1]
+    return list1
 
 def interpunction(li):
     """The function modifies elements in a list. If there is at least one of symbols .,;:?! at end of a string, this symbol is going to be moved to the opposite \
@@ -25,7 +25,7 @@ def interpunction(li):
         i = list(a)
         again = True
         #Following section iterates until any "invalid" (as mentioned above) character remains at the end of a string 
-        while again == True:
+        while again:
             if i[-1] == "." or i[-1] == "!" or i[-1] == "?" or i[-1] == "," or i[-1] == ";" or i[-1] == ":":
                 #Putting the character to the first position in a string (=in front of the string)
                 i.insert(0, i[-1])
@@ -51,7 +51,7 @@ def capital_letter(li):
     new_li = []
     for a in li:
         i = list(a)
-        if i[0].isupper() == True:
+        if i[0].isupper():
             i[0] = i[0].lower()
             i[-1] = i[-1].upper()
             a = ''.join(i)
@@ -84,17 +84,17 @@ def write_text(adress, text_to_be_written):
 try:
     text = load_data("text.txt")
 except:
-    text = input("File text.txt was not found in the same folder from which you are running this program, or it fail to load the file. You can enter text manually:")
+    text = input("File text.txt was not found in the same folder from which you are running this program, or file loading failed . You can enter text manually:")
 print(f"Following text is going to be modified (words are going to be written in reverse order): {text}")
 capitals_yes = input("Do you wish to change capital letters in order to make the text readable in right-left direction? Enter 1 for YES.")
 #Splits the text by whitespaces (including more whitespaces, including tabs)
 text_list = text.split()
-text_f = list_reverse(text_list)
-text_f = interpunction(text_f)
+text_list = list_reverse(text_list)
+text_list = interpunction(text_list)
 if capitals_yes == "1":
-    text_f = capital_letter(text_f)
-text_f_joined = ' '.join(text_f)
-print(text_f_joined)
+    text_list = capital_letter(text_list)
+text_list_joined = ' '.join(text_list)
+print(text_list_joined)
 write_yes = input("Do you wish to save generated reversed text to new text file? Enter 1 for YES.")
 if write_yes == "1":
-    write_text("reversed.txt", text_f_joined)
+    write_text("reversed.txt", text_list_joined)
